7d11f2aa7e71016e9c74881f11ece387949ebfe2
almightyprogrammer <107855747+almightyprogrammer@users.noreply.github.com>
2025-05-17 20:00:17 +1200

    Environment variables for settings.py for security issues

--- forumProject/settings.py
+++ forumProject/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+#
 
 # Internationalization
 LANGUAGE_CODE = 'en-us'
